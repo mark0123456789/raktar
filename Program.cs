295bb27a3ff23064f3aa317d54bcffc65c756460
mark <piringerm@kkszki.hu>
2024-11-29 12:58:25 +0100

    this is the stop program

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace raktar
+{
+    internal class Program
+    {
+        public static Connect conn = new Connect();
+        public static void Eszkozok()
+        {
+            conn.Connection.Open();
+            string sql = "SELECT * FROM `eszkozok`";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+            dr.Read();
+
+            do
+            {
+                var eszkozok = new
+                {
+                    Id = dr.GetInt32(0),
+                    AlkatreszNev = dr.GetString(1),
+                    AlkatreszAr = dr.GetInt32(2),
+                    AlkatreszGyarto = dr.GetString(3),
+                    AlkatreszTipus = dr.GetString(4),
+                    Felveve = dr.GetDateTime(5)
+                };
+
+                Console.WriteLine($"A raktárban elérhető termékek: {eszkozok.AlkatreszNev}, {eszkozok.AlkatreszAr}Ft, {eszkozok.AlkatreszGyarto}, {eszkozok.AlkatreszTipus}, Adatbázisba véve: {eszkozok.Felveve}");
+            }
+            while (dr.Read());
+
+
+
+            dr.Close();
+
+
+
+            conn.Connection.Close();
+        }
+
+
+        static void Main(string[] args)
+        {
+            Eszkozok();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/Connect.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+
+namespace raktar
+{
+    public class Connect
+    {
+        public MySqlConnection Connection;
+        public string Host;
+        public string Database;
+        public string User;
+        public string Password;
+        public string ConnectionString;
+
+        public Connect()
+        {
+            Host = "localhost";
+            Database = "raktar";
+            User = "root";
+            Password = "";
+
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + User + ";PASSWORD=" + Password + ";SslMode=None";
+
+            Connection = new MySqlConnection(ConnectionString);
+        }
+
+    }
+}
